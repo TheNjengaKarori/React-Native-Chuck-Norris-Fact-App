4e9a295b53af83330dbeb6f46c2e929315353a1b
Njengak11 <njengak11@gmail.com>
2021-10-08 10:05:41 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ImageBackground,TouchableOpacity, } from 'react-native';
 
 export default function App() {
+
+let [chuck,setChuck] = React.useState('')
+
+const fetchApiCall = () => {
+  fetch("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random", {
+    "method": "GET",
+    "headers": {
+      "accept": "application/json",
+      "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+      "x-rapidapi-key": "5d2b81df15msh88151fcd965cdf8p188f3cjsn2dddb2192314"
+    }
+  })
+  .then(response => response.json())
+  .then(response => {
+    setChuck(response.value);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+	}
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground  source={require('./assets/images/norris.jpg')}
+       style={styles.image}>
+      
+      <View style={styles.textContainer}>
+      <Text style={styles.text}>{chuck}</Text>
+      </View>
+        <TouchableOpacity style={styles.button}
+        onPress={fetchApiCall}
+        >
+         <Text style={styles.btnText}>GET A CHUCK NORRIS FACT</Text>
+        </TouchableOpacity>
+        </ImageBackground>
     </View>
   );
 }
@@ -14,8 +47,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    fontSize:70,
+    color:'white',
+    bottom:286,
+    textAlign:'center',
+  },
+image:{
+  justifyContent:'center', 
+  width:'100%',
+  height:'100%',
+  resizeMode:'cover'
+ 
+},
+button:{
+  backgroundColor:'#ff8c00',
+  height:80,
+  width:'80%',
+  borderRadius:10,
+  alignItems:'center',
+  justifyContent:'center',
+  marginLeft:40,
+  marginRight:40,
+  top:200
+},
+btnText:{
+  color:'white',
+  fontSize:25,
+  fontWeight:'bold'
+},
+textContainer:{
+  alignItems:'center',
+  justifyContent:'center',
+},
+text:{
+  color:'white',
+  fontSize:30,
+  fontWeight:'bold',
+  textAlign:'center'
+}
 });
+
+
+
